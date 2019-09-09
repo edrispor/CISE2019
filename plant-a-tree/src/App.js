25e5987d6e47ff8afb34bae04c9139fc9a7ff967
Silva <naveendesilva88@gmail.com>
2019-09-09 20:35:02 +1200

    setting up react-router

--- plant-a-tree/src/App.js
+++ plant-a-tree/src/App.js
@@ -1,13 +1,30 @@
 import React from "react";
 import "./App.css";
+import { Route, Switch } from "react-router-dom";
+import Details from "./pages/Details";
+import Error from "./pages/Error";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Search from "./pages/Search";
+import Tree from "./pages/Tree";
+
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1> Olla amigos get to work.</h1>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/details" component={Details} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/search" component={Search} />
+        <Route exact path="/tree" component={Tree} />
+        <Route component={Error} />
+      </Switch>
+    </>
   );
 }
 
---
+++ plant-a-tree/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div>
+      <h1>Plan a Tree </h1>
+    </div>
+  );
+}
