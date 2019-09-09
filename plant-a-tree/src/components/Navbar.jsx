1938054e3bc8e842634dbe7db0d3b326726f1f97
Silva <naveendesilva88@gmail.com>
2019-09-09 21:32:14 +1200

    Creating Navbar

--- plant-a-tree/src/components/Navbar.jsx
+++ plant-a-tree/src/components/Navbar.jsx
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import logo from "../images/logo.svg";
+export default class Navbar extends Component {
+  // <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/"             title="Flaticon">www.flaticon.com</a></div>
+  render() {
+    return (
+      <nav className="navbar">
+        <div className="nav-center">
+          <div className="nav-header">
+            <Link to="/">
+              <img src={logo} alt="Plant a Tree" />
+            </Link>
+          </div>
+          <ul id="horizontal-list">
+            <li>
+              <Link to="/"> Home</Link>
+            </li>
 
-export default function Navbar() {
-  return (
-    <div>
-      <h1>Plan a Tree </h1>
-    </div>
-  );
+            <li>
+              <Link to="/search"> Search</Link>
+            </li>
+            <li>
+              <Link to="/details"> Details</Link>
+            </li>
+            <li>
+              <Link to="/login"> Login</Link>
+            </li>
+            <li>
+              <Link to="/register"> Register</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
 }
