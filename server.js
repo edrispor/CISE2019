6dbd48746738874f2e8be3b711441c90b86bccff
Bhasura <brian.desilva5@gmail.com>
2019-09-22 02:37:07 +1200

    solution to crashing server

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ app.use(express.static(path.join(__dirname, "plant-a-tree/build")));
 
 const SELECT_ALL_PRODUCTS_QUERY = "SELECT * FROM products";
 
-const connection = mysql.createConnection({
+let pool = mysql.createPool({
+  connectionLimit: 10,
   host: "us-cdbr-iron-east-02.cleardb.net",
   user: "b02474a9fac601",
   password: "15251463",
@@ -24,21 +25,18 @@ app.get("/", (req, res) => {
   res.send("go to /products");
 });
 
-connection.connect(err => {
-  if (!err) console.log("DB connection succeded");
-  else
-    console.log(
-      "DB Connection failed \n Error : " + JSON.stringify(err, undefined, 2)
-    );
-});
-
 app.get("/products", (req, res) => {
-  connection.query(SELECT_ALL_PRODUCTS_QUERY, (err, results) => {
+  pool.getConnection(function(err, conn) {
     if (err) {
-      return res.send(err);
+      res.send("Error occured");
     } else {
-      return res.json({
-        data: results
+      conn.query(SELECT_ALL_PRODUCTS_QUERY, function(err2, records, fields) {
+        if (!err2) {
+          res.json({
+            data: records
+          });
+        }
+        conn.release();
       });
     }
   });
