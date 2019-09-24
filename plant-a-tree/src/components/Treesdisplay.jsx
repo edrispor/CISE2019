e01cd6984dfcc088355cacd8dc060053f7092df4
edrispor <edris.por@gmail.com>
2019-09-25 01:12:54 +1200

    added NZ kauri tree image

--- plant-a-tree/src/components/Treesdisplay.jsx
+++ plant-a-tree/src/components/Treesdisplay.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import NZ_Kauri from "../images/Kauri_tree.jpg";
+import cat from "../images/cat.jpeg";
 export default function Treesdisplay({ tree }) {
   let {
     product_id,
@@ -15,13 +17,26 @@ export default function Treesdisplay({ tree }) {
     growth_rate,
     description,
     photo_link,
-    tags
+    tags,
+    slug
   } = tree;
+
+  function importimages(product_id) {
+    if (product_id == 1) {
+      return (
+        <Link to={`/items/trees/${slug}`}>
+          {""}
+          {product_name} <img src={NZ_Kauri} alt={cat}></img>{" "}
+        </Link>
+      );
+    }
+  }
   return (
     <article>
       <div>
         {product_id} | {product_name} | {latin_name} | {product_price} |{" "}
       </div>
+      <div> This should show images {importimages(product_id)}</div>
     </article>
   );
 }
