ac90c40ac96404187be43aa3593f1078e171f8dc
naveen <naveendesilva88@gmail.com>
2019-09-24 17:17:46 +1200

    cleaning up code for master

--- plant-a-tree/src/components/Tools.jsx
+++ plant-a-tree/src/components/Tools.jsx
@@ -38,27 +38,6 @@ export default class Tools extends Component {
     return tempTools;
   }
 
-  renderTools = ({ product_id, product_name, product_price }) => (
-    <div key={product_id}>
-      {product_id} : {product_name} : {product_price} |
-    </div>
-  );
-  //this is an example of how to store the item into an object
-  justchecking() {
-    const { tools } = this.state;
-    let tempTools = tools.map(tool => {
-      let id = tool.product_id;
-      let name = tool.product_name;
-      let price = tool.product_price;
-      let description = tool.description;
-      let photo = tool.photo_link;
-      let tags = tool.tags;
-      let slug = tool.slug;
-      let toolobj = { id, name, description, price, photo, tags, slug };
-      return toolobj;
-    });
-    return tempTools;
-  }
   render() {
     let { tools } = this.state;
     console.log(tools);
@@ -73,14 +52,4 @@ export default class Tools extends Component {
       </section>
     );
   }
-  /*
-  render() {
-    const { tools } = this.state;
-    //x will contain the item object
-    let x = this.justchecking();
-    console.log(x);
-    //console.log([0].description);
-    return <div>{tools.map(this.renderTools)}</div>;
-  }
-  */
 }
