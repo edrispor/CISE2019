7fa000e629474b3503e8ff921c6700bc0a3891f6
Bhasura <brian.desilva5@gmail.com>
2019-09-27 18:11:38 +1200

    login and account working

--- plant-a-tree/src/pages/Login.js
+++ plant-a-tree/src/pages/Login.js
@@ -1,12 +1,149 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
-export default function Login() {
-  return (
-    <div className="pagelayout">
-      <div className="login-page">
-        <p>This is Login Page</p>
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      userprofile: "",
+      show: false,
+      show2: false
+    };
+    this.toggleDiv = this.toggleDiv.bind(this);
+    this.toggleDiv2 = this.toggleDiv2.bind(this);
+  }
+  componentDidMount() {
+    this.getUsers();
+  }
+
+  getUsers = _ => {
+    fetch("/data")
+      .then(response => response.json())
+      .then(response => this.setState({ users: response.data }))
+      .catch(err => console.error(err));
+  };
+  toggleDiv = e => {
+    e.preventDefault();
+    var resultUser = this.state.users.findIndex(
+      entry => entry.username === this.inputusername.value
+    );
+    var resultPass = this.state.users.findIndex(
+      entry => entry.pass === this.inputpass.value
+    );
+
+    if (resultUser === resultPass) {
+      const { show } = this.state;
+      this.setState({ show: true });
+    } else {
+      console.log("not passed mate");
+    }
+  };
+  toggleDiv2 = e => {
+    e.preventDefault();
+    var resultUser = this.state.users.findIndex(
+      entry => entry.username === this.inputusername.value
+    );
+    var resultPass = this.state.users.findIndex(
+      entry => entry.pass === this.inputpass.value
+    );
+
+    if (resultUser === resultPass) {
+      const { show } = this.state;
+      this.setState({ show: true });
+    } else {
+      console.log("not passed mate");
+    }
+  };
+
+  render() {
+    const { users, userprofile } = this.state;
+    if (userprofile === "valid") {
+      console.log("anlfsdnfal");
+    } else {
+      console.log("nuuuuuuuu");
+    }
+
+    const listItems = users.map(d => (
+      <li key={d.username}>
+        {d.username}>{d.email}
+      </li>
+    ));
+    return (
+      <div className="pagelayout">
+        <div className="login-page">
+          <h1>Login</h1>
+          <form>
+            <div className="form-group">
+              <input
+                className="form-control"
+                type="text"
+                id="username"
+                ref={inusername => (this.inputusername = inusername)}
+                value={this.props.inputValue}
+                onChange={this.props.inputChanged}
+                placeholder="Enter a username"
+              />
+            </div>
+            <div className="form-group">
+              <input
+                className="form-control"
+                type="text"
+                id="pass"
+                ref={inpass => (this.inputpass = inpass)}
+                placeholder="Enter password"
+              />
+            </div>
+            <input type="checkbox" name="rememberme" value="Remember" />{" "}
+            Remember me <br /> <br />
+            <Link to="/login/forget-password">Forgot Password</Link> <br />{" "}
+            <br />
+            <button
+              width="135px"
+              color="#F4FF77"
+              radius="50px"
+              class="btnitem"
+              onClick={this.toggleDiv}
+            >
+              > Login
+            </button>
+          </form>
+          {this.state.show && <Box />}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+class Box extends Component {
+  render() {
+    return (
+      <div>
+        {" "}
+        Login Succesfull... Click <Link to="/items">here</Link> to browse our
+        collection
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    inputValue: state.inputValue
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    inputChanged: evt => {
+      console.log("changed", evt.target.value);
+      const action = { type: "INPUT_CHANGE", text: evt.target.value };
+      dispatch(action);
+    }
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Login);
