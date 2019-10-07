2bd6fa13d2c0ef706ff941de12f7225f20c72790
naveen <naveendesilva88@gmail.com>
2019-10-07 14:13:38 +1300

    fixing up items page

--- plant-a-tree/src/components/Items.jsx
+++ plant-a-tree/src/components/Items.jsx
@@ -88,6 +88,21 @@ export default class Items extends Component {
             <div className="item">
               <div className="itemdetails">
                 <h2>{item.product_name}</h2>
+                <br></br>
+                <p1>{item.latin_name}</p1>
+                <br />
+                <p2>type: {item.tree_type}</p2>
+                <p1>maintenaince: {item.maintenaince}</p1>
+                <br />
+                <p1>sunlight: {item.sunlight}</p1>
+                <br />
+                <p1>height: {item.height}</p1>
+                <br />
+                <p1>soil: {item.soil_drainage}</p1>
+                <br />
+                <p1>gr: {item.growth_rate}</p1>
+                <br />
+
                 <p1>Click to find out more.</p1>
                 <span className="price">${item.product_price}</span>
                 <button
